229e0ebbdba59b0f6e9aa003f48ab46829f65bb6
jibran <jibranshaheer2@gmail.com>
2025-11-19 04:07:00 +0500

    updated

---
+++ src/Views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import HeroComponent from '@/components/HomePage/HeroComponent.vue';
+
+
+</script>
+<template>
+    
+    <HeroComponent />
+
+</template>
---
+++ src/components/HomePage/HeroComponent.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+</script>
+<template>
+    <section class="relative h-[15vh] bg-cover bg-center" style="background-image: url('/src/images/bg2.jpg');">
+        <div class="absolute inset-0 bg-black/80"></div>
+        <h1
+            class="relative text-[#FFC827] text-4xl font-bold text-center  w-[500px] py-2 rounded-lg !mx-auto !py-[45px]">
+            247 Carz Business Accounts
+        </h1>
+    </section>   
+    <section class="max-w-[1536px] !px-6 !py-12 lg:!py-20 !mx-auto">
+        <div class="grid grid-cols-1 lg:grid-cols-2 gap-12">
+            <div>
+                <h2 class="text-4xl font-bold text-[#241F21]">Your Business, Our Priority</h2>
+                <p class="text-lg font-light !mt-3">24/7 Radio Carz is the largest private hire company in Birmingham and surrounding areas. If you are looking for a business account we can ensure you receive priority service even in the busiest periods.</p>
+            </div>
+        </div>
+    </section>
+</template>
